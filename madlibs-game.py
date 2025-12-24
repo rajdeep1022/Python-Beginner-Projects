b16a3245bf64e16bbc4766c18a9a0137c8c412ce
Rajdeep Chakraborty <rajdeepchakraborty1022@gmail.com>
2025-12-24 20:48:50 +0530

    Add basic Madlibs game implementation

---
+++ madlibs-game.py
@@ -0,0 +1,16 @@
+# Madlibs Game
+# Word game where you create a story
+# By filling in blanks with random words
+
+
+adj1 = input("Enter an adjective(Description): ")
+noun1 = input("Enter a noun (Person, Place, Thing):")
+adj2 = input("Enter an adjective(Description): ")
+verb1 = input("Enter a verb(ending with 'ing'): ")
+adj3 = input("Enter an adjective(Description): ")
+
+print("")
+print(f"Today I went to a {adj1} place.")
+print(f"In a temple, I saw {noun1}.")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}!")
