e7ebd551d4a9fd2cf10f003d018c8f751a7cdbfd
Rajdeep Chakraborty <rajdeepchakraborty1022@gmail.com>
2026-01-01 23:27:15 +0530

    Fix variable casing for unit comparison

--- temperature-converter.py
+++ temperature-converter.py
@@ -2,12 +2,12 @@
 
 unit = input("Celsius or Fahrenheit (C/F): ")
 temp = float(input("Enter the Temperature: "))
-UNIT = unit.capitalize()
-if UNIT == "C":
+Unit = unit.capitalize()
+if Unit == "C":
     temp = round((9*temp)/5 + 32, 1)
     print(f"Your temperature is: {temp} °F")
-elif UNIT == "F":
+elif Unit == "F":
     temp = round((temp - 32)* 5 / 9, 1)
     print(f"Your temperature is: {temp} °C")
 else:
-    print(f"{UNIT} isn't a valid unit")
+    print(f"{Unit} isn't a valid unit")
